6d0eb86ffa128f64ddec6e65ca391274a8502fa8
김해동 <haedoang@hanatour.com>
2025-10-15 21:50:02 +0900

    [ORD-2] Test 용 코드 제거
    - rm) CommandLineRunner 테스트 로직 제거

--- order/src/main/kotlin/io/readingrecord/order/OrderApplication.kt
+++ order/src/main/kotlin/io/readingrecord/order/OrderApplication.kt
@@ -1,23 +1,14 @@
 package io.readingrecord.order
 
-import org.springframework.boot.CommandLineRunner
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
-import org.springframework.context.annotation.Bean
-import org.springframework.kafka.core.KafkaTemplate
 
 @SpringBootApplication(scanBasePackages = ["io.readingrecord"])
-class OrderApplication {
-
-    @Bean
-    fun init(kafkaTemplate: KafkaTemplate<String, Any>): CommandLineRunner {
-        return CommandLineRunner {
-            println(kafkaTemplate)
-        }
-    }
-}
+class OrderApplication
 
 fun main(args: Array<String>) {
     runApplication<OrderApplication>(*args)
 }
+
+
 
